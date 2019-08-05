31b9010a3e22491f26d8601d8f4543f82d735090
Marcus <marcusporama@gmail.com>
2019-08-05 00:41:57 -0300

    ajustando formlogin com nativebase

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,37 +1,63 @@
 import React, { Fragment } from "React";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import {
+  Body,
+  Title,
+  Header,
+  Container,
+  Left,
+  Icon,
+  Input,
+  Item
+} from "native-base";
 
 export default props => (
-  <View style={styles.geral}>
-    <View style={styles.topoNome}>
-      <Text style={styles.nameStyle}>Whatapp Clone</Text>
-    </View>
-    <View style={styles.inputes}>
-      <TextInput
-        style={styles.styleInput}
-        keyboardType="email-address"
-        placeholder="E-mail"
-      />
-    </View>
-    <View style={{ flex: 2, paddingTop: 15 }}>
-      <TextInput
-        style={styles.styleInput}
-        returnKeyType="done"
-        placeholder="Senha"
-      />
-      <View style={styles.topoNome}>
-        <Text style={styles.styleCadastro}>
-          Ainda não tem cadastro? Cadastre-se
-        </Text>
+  <Container>
+    <Header androidStatusBarColor="#115E54" style={styles.header}>
+      <Left />
+      <Body>
+        <Title>WhatsApp Clone</Title>
+      </Body>
+    </Header>
+    <View style={styles.geral}>
+      <View style={styles.topoNome} />
+      <View style={styles.inputes}>
+        <Item>
+          <Icon name="email" type="MaterialCommunityIcons" />
+          <Input
+            style={styles.styleInput}
+            keyboardType="email-address"
+            placeholder="E-mail"
+          />
+        </Item>
+      </View>
+      <View style={{ flex: 2, paddingTop: 15 }}>
+        <Item>
+          <Icon name="textbox-password" type="MaterialCommunityIcons" />
+          <Input
+            style={styles.styleInput}
+            returnKeyType="done"
+            placeholder="Senha"
+          />
+        </Item>
+        <View style={styles.topoNome}>
+          <Text style={styles.styleCadastro}>
+            Ainda não tem cadastro? Cadastre-se
+          </Text>
+        </View>
+      </View>
+      <View style={{ flex: 2 }}>
+        <Button title="Acessar" color="#115E54" onPress={() => false} />
       </View>
     </View>
-    <View style={{ flex: 2 }}>
-      <Button title="Acessar" color="#115E54" onPress={() => false} />
-    </View>
-  </View>
+  </Container>
 );
 
 const styles = StyleSheet.create({
+  header: {
+    color: "#115E54",
+    backgroundColor: "#115E54"
+  },
   geral: {
     flex: 1,
     padding: 10
@@ -50,8 +76,8 @@ const styles = StyleSheet.create({
   styleInput: {
     fontSize: 20,
     height: 45,
-    borderColor: "#115E54",
-    borderWidth: 1
+    borderBottomColor: "#000000",
+    borderBottomWidth: 1
   },
   styleCadastro: {
     fontSize: 15,
