6931f0c4ada2b37e9b46f22d477767daeff090f4
Marcus <marcusporama@gmail.com>
2019-08-04 22:45:55 -0300

    Criação do FormLogin

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,12 +1,18 @@
 import React from "React";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
 export default props => (
   <View>
     <View 
-        <Text>WhatappClone</Text>
+        <Text>Whatapp Clone</Text>
     </View>
-    <View />
-    <View />
+    <View>
+        <TextInput placeholder="E-mail" />
+        <TextInput placeholder="Senha" />
+        <Text>Ainda não tem cadastro? Cadastre-se</Text>
+    </View>
+    <View>
+        <Button title="Acessar" onPress={() => false} />
+    </View>
   </View>
 );
