1d26fe4d0cf54718830d22c0069948108b902746
Marcus <marcusporama@gmail.com>
2019-08-04 23:17:52 -0300

    Estilização do formlogin

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,18 +1,50 @@
 import React from "React";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 export default props => (
-  <View>
-    <View>
-      <Text>Whatapp Clone</Text>
+  <View style={styles.geral}>
+    <View style={styles.topoNome}>
+      <Text style={styles.nameStyle}>Whatapp Clone</Text>
     </View>
-    <View>
-      <TextInput placeholder="E-mail" />
-      <TextInput placeholder="Senha" />
-      <Text>Ainda não tem cadastro? Cadastre-se</Text>
+    <View style={{ flex: 2 }}>
+      <TextInput style={styles.styleInput} placeholder="E-mail" />
+      <TextInput style={styles.styleInput} placeholder="Senha" />
+      <View style={styles.topoNome}>
+        <Text style={styles.styleCadastro}>
+          Ainda não tem cadastro? Cadastre-se
+        </Text>
+      </View>
     </View>
-    <View>
-      <Button title="Acessar" onPress={() => false} />
+    <View style={{ flex: 2 }}>
+      <Button title="Acessar" color="#115E54" onPress={() => false} />
     </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  geral: {
+    flex: 1,
+    padding: 10
+  },
+  topoNome: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  nameStyle: {
+    color: "#115E54",
+    fontSize: 25,
+    fontStyle: "italic",
+    fontWeight: "bold"
+  },
+  styleInput: {
+    fontSize: 20,
+    height: 45
+  },
+  styleCadastro: {
+    fontSize: 15,
+    height: 45,
+    fontStyle: "italic",
+    fontWeight: "bold"
+  }
+});
